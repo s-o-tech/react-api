50a495525b2c0d50c9060d9f4cf9924c2f613990
s-o-tech <s.otani@atomitech.jp>
2020-09-06 12:22:32 +0900

    add signin.js

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
+var signinRouter = require('./routes/signin');
 
 var knex = require('./db/knex');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 
 var app = express();
 
@@ -22,6 +28,53 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy({
+  emailField: "email", 
+  passwordField: "password", 
+},function(email, password, done) {
+  knex.select('*').from('users').where({email:email})
+  .then(function(rows){
+    let users = Object.values(JSON.parse(JSON.stringify(rows)));
+    if(!users[0] || users.length != 1){
+      return done(null,false,{message:'Invalid UserName'});
+    }
+    else if(users[0].password != password){
+      return done(null,false,{message:"Invalid Password"});
+    }
+    else{
+      return done(null,users[0]);
+    }
+  })
+  .catch(function(err){
+    console.error(err);
+    return done(null,false,{message:'error'});
+  })
+}
+));
+
+//route
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup',signupRouter);
