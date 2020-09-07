0b33dc5c1da6efbf484f0a7adf1faf7001d5e050
Daisuke HIGASHI <me@lulichn.net>
2020-09-08 03:04:14 +0900

    format

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,4 @@
-const environment = process.env.ENVIRONMENT || 'development'
-const config = require('../knexfile.js')[environment];
-module.exports = require('knex')(config);
+const environment = process.env.ENVIRONMENT || "development";
+const config = require("../knexfile.js")[environment];
+
+module.exports = require("knex")(config);
--- routes/signup.js
+++ routes/signup.js
@@ -1,44 +1,61 @@
-let express = require('express'),
-    router = express.Router(),
-    knex = require('../db/knex');
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/knex");
 
-router.get('/',function(req,res,next){
-    res.render('signup',{title:'Sign Up',errorMessage:[],isAuth:false/*req.isAuthenticated()*/});
+router.get("/", function (req, res, next) {
+  res.render("signup", {
+    title: "Sign Up",
+    errorMessage: [],
+    isAuth: false /* req.isAuthenticated() */,
+  });
 });
 
-router.post('/', function(req,res,next){
-    let username = req.body.username,
-        password = req.body.password,
-        passwordConfirm = req.body.passwordConfirm,
-        email = req.body.email,
-        errorMessage = [];
-    if(username == ''){
-        errorMessage.push("username can't be blank");
-    }
-    if(password == ''){
-        errorMessage.push("password can't be blank");
-    }
-    if(email == '' || !(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) ){
-        errorMessage.push("email is invalid");
-    }
-    if(password != passwordConfirm){
-        errorMessage.push("Password doesn't match.");
-    }
+router.post("/", function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const passwordConfirm = req.body.passwordConfirm;
+  const email = req.body.email;
+  const errorMessage = [];
+  if (username == "") {
+    errorMessage.push("username can't be blank");
+  }
+  if (password == "") {
+    errorMessage.push("password can't be blank");
+  }
+  if (
+    email == "" ||
+    !/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    errorMessage.push("email is invalid");
+  }
+  if (password != passwordConfirm) {
+    errorMessage.push("Password doesn't match.");
+  }
 
-    if(errorMessage.length != 0){
-        res.render('signup',{title:'Sign up',errorMessage:errorMessage,isAuth:false});
-    }
-    else {
-        knex('user').insert({id:0,name:username,password:password,email:email})
-        .then(function(resp){
-            //あとで変更予定
-            res.redirect('/');
-        })
-        .catch(function(err){
-            console.error(err);
-            res.render('signup',{title:'Sign up',errorMessage:[`This username(${username}) is already used`],isAuth:false});
-        }) 
-    }
-}
-);
+  if (errorMessage.length != 0) {
+    res.render("signup", {
+      title: "Sign up",
+      errorMessage: errorMessage,
+      isAuth: false,
+    });
+  } else {
+    knex("user")
+      .insert({ id: 0, name: username, password: password, email: email })
+      .then(function (resp) {
+        // あとで変更予定
+        res.redirect("/");
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: [`This username(${username}) is already used`],
+          isAuth: false,
+        });
+      });
+  }
+});
+
 module.exports = router;
