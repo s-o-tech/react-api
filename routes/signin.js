da33949c38ed1db1ef91cd4f2f2fbbd2841cf16f
s-o-tech <s.otani@atomitech.jp>
2020-09-12 05:38:30 +0900

    fix const

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,6 @@
-let express = require('express');
-let router = express.Router();
-let passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 
 router.get('/',function(req,res,next){
     res.render('signin',{title:"Sign in",isAuth:req.isAuthenticated(),errorMessage:req.flash('error')});
