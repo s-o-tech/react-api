ebfce23b0a9b9923065c9dfc41b3ec8f38ce09c9
s-o-tech <s.otani@atomitech.jp>
2020-09-19 12:01:57 +0900

    add delete user

--- public/js/deleteuser.js
+++ public/js/deleteuser.js
@@ -1,9 +1,9 @@
-$("#deleteModal").on('show.bs.modal', function (event) {
-    const delete_target = $(event.relatedTarget) // Button that triggered the modal
-    const recipient = delete_target.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    const modal = $(this)
-    modal.find("#deleteUser").text(recipient);
-    modal.find("#deleteTarget").val(recipient);
+$("#deleteModal").on("show.bs.modal", function (event) {
+  const deleteTarget = $(event.relatedTarget); // Button that triggered the modal
+  const recipient = deleteTarget.data("whatever"); // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  const modal = $(this);
+  modal.find("#deleteUser").text(recipient);
+  modal.find("#deleteTarget").val(recipient);
 });
--- routes/userlist.js
+++ routes/userlist.js
@@ -3,27 +3,61 @@ const router = express.Router();
 const knex = require("../db/knex");
 
 router.get("/", function (req, res, next) {
-  let currentPage;
-  if (req.query.page === undefined) {
-    currentPage = 1;
-  } else {
-    currentPage = parseInt(req.query.page);
-  }
-  knex("users")
-    .paginate({ perPage: 10, currentPage: currentPage, isLengthAware: true })
-    .then(function (result) {
-      const userRows = JSON.parse(JSON.stringify(result.data));
-      const pagination = result.pagination;
+  if (req.isAuthenticated()) {
+    let currentPage;
+    if (req.query.page === undefined) {
+      currentPage = 1;
+    } else {
+      currentPage = parseInt(req.query.page);
+    }
+    knex("users")
+      .paginate({ perPage: 10, currentPage: currentPage, isLengthAware: true })
+      .then(function (result) {
+        const userRows = JSON.parse(JSON.stringify(result.data));
+        const pagination = result.pagination;
 
-      res.render("userlist", {
-        title: "Userlist",
-        message: "",
-        isAuth: req.isAuthenticated(),
-        users: userRows,
-        pagination: pagination,
-        isAdmin: req.user.isAdmin,
+        res.render("userlist", {
+          title: "Userlist",
+          message: "",
+          isAuth: req.isAuthenticated(),
+          users: userRows,
+          pagination: pagination,
+          isAdmin: req.user.isAdmin,
+        });
+      })
+      .catch(function (err) {
+        // error処理は変更予定
+        console.error(err);
+        res.redirect("/userlist");
       });
-    });
+  } else {
+    res.redirect("/signin");
+  }
 });
+
+router.post(
+  "/",
+  function (req, res, next) {
+    // admin以外のリクエストは分岐させる
+    if (req.isAuthenticated() && req.user.isAdmin) {
+      const target = req.body.target;
+      knex("users")
+        .where({ name: target })
+        .del()
+        .then(function (result) {
+          next();
+        })
+        .catch(function (err) {
+          console.error(err);
+          next();
+        });
+    } else {
+      next();
+    }
+  },
+  function (req, res) {
+    res.redirect("/userlist");
+  }
+);
 
 module.exports = router;
