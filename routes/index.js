035efa647b600ac82a96c30706aa56209c1f35ec
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-09-29 14:13:24 +0900

    ルートパスにポストページを移動

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("../db/knex");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", {
-    title: "MicroPost",
-    message: "",
-    isAuth: req.isAuthenticated(),
-  });
+  if (req.isAuthenticated()) {
+    const userId = req.user.id;
+    const userName = req.user.name;
+    let total = "";
+    let currentPage;
+
+    if (req.query.page === undefined) {
+      currentPage = 1;
+    } else {
+      currentPage = parseInt(req.query.page);
+    }
+
+    knex("microposts")
+      .where("user_id", userId)
+      .then(function (result) {
+        total = result.length;
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "",
+          errorMessage: [err.sqlMessage],
+          isAuth: req.isAuthenticated(),
+          userId: userId,
+        });
+      });
+
+    knex("microposts")
+      .where("user_id", userId)
+      .paginate({ perPage: 10, currentPage: currentPage, isLengthAware: true })
+      .then(function (result) {
+        const microposts = JSON.parse(JSON.stringify(result.data));
+        const pagination = result.pagination;
+        console.debug(microposts);
+        res.render("index", {
+          title: "",
+          message: "",
+          isAuth: req.isAuthenticated(),
+          userId: userId,
+          userName: userName,
+          microposts: microposts,
+          total: total,
+          pagination: pagination,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("index", {
+          title: "",
+          errorMessage: [err.sqlMessage],
+          isAuth: req.isAuthenticated(),
+          userId: userId,
+        });
+      });
+  } else {
+    res.render("index", {
+      title: "MicroPost",
+      message: "",
+      isAuth: req.isAuthenticated(),
+    });
+  }
 });
 
 router.use("/users", require("./users"));
@@ -15,8 +72,8 @@ router.use("/signup", require("./signup"));
 router.use("/signin", require("./signin"));
 router.use("/logout", require("./logout"));
 router.use("/edit", require("./edit"));
-router.use("/home", require("./postList"));
-router.use("/profile", require("./postList"));
+router.use("/home", require("./profile"));
+router.use("/users/*", require("./profile"));
 router.use("/post", require("./post"));
 router.use("/deletePost", require("./deletePost"));
 
