b5b21f706fc599f0b269cf667bc68c75d8dfde94
Kenta Iwadate <k.iwadate@atomitech.jp>
2020-10-01 00:20:51 +0900

    フォロー一覧ページ追加

--- routes/following.js
+++ routes/following.js
@@ -3,34 +3,63 @@ const router = express.Router();
 const knex = require("../db/knex");
 
 router.get("/", function (req, res, next) {
+  console.debug("following");
   const userId = req.user.id;
   const userName = req.user.name;
-
-  let currentPage;
-  if (req.query.page === undefined) {
-    currentPage = 1;
-  } else {
-    currentPage = parseInt(req.query.page);
-  }
+  let totalFollowing = "";
+  let totalFollowers = "";
 
   knex("relationships")
     .where("follower_id", userId)
-    .paginate({ perPage: 10, currentPage: currentPage, isLengthAware: true })
     .then(function (result) {
-      const follower = JSON.parse(JSON.stringify(result.data));
-      const pagination = result.pagination;
-      res.render(url, {
+      totalFollowing = result.length;
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("index", {
+        title: "",
+        errorMessage: [err.sqlMessage],
+        isAuth: req.isAuthenticated(),
+        userId: userId,
+      });
+    });
+
+  knex("relationships")
+    .where("followed_id", userId)
+    .then(function (result) {
+      totalFollowers = result.length;
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("index", {
+        title: "",
+        errorMessage: [err.sqlMessage],
+        isAuth: req.isAuthenticated(),
+        userId: userId,
+      });
+    });
+
+  knex("relationships")
+    .join("users", "relationships.followed_id", "=", "users.id")
+    .where("follower_id", userId)
+    .then(function (result) {
+      const following = JSON.parse(JSON.stringify(result));
+      console.debug("result");
+      console.debug(following);
+      res.render("following", {
         title: "",
         message: "",
         isAuth: req.isAuthenticated(),
         userName: userName,
-        follower: follower,
-        pagination: pagination,
+        userId: userId,
+        totalFollowing: totalFollowing,
+        totalFollowers: totalFollowers,
+        following: following,
       });
     })
     .catch(function (err) {
       console.error(err);
-      res.render(url, {
+      res.render("following", {
         title: "",
         errorMessage: [err.sqlMessage],
         isAuth: req.isAuthenticated(),
