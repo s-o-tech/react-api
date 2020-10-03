5abfd6b88491b567bd4b6fe8c926f061dba16d91
s-o-tech <s.otani@atomitech.jp>
2020-10-03 15:16:26 +0900

    delete local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,4 @@
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const bcrypt = require("bcrypt");
 const knex = require("../db/knex");
 
 module.exports = function (app) {
@@ -31,34 +29,4 @@ module.exports = function (app) {
         return done(null, false);
       });
   });
-
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "email",
-        passwordField: "password",
-      },
-      function (username, password, done) {
-        knex
-          .select("*")
-          .from("users")
-          .where({ email: username })
-          .then(function (rows) {
-            const users = Object.values(JSON.parse(JSON.stringify(rows)));
-
-            if (!users[0] || users.length !== 1) {
-              return done(null, false, { message: "Invalid Email" });
-            } else if (!bcrypt.compareSync(password, users[0].password)) {
-              return done(null, false, { message: "Invalid Password" });
-            } else {
-              return done(null, users[0]);
-            }
-          })
-          .catch(function (err) {
-            console.error(err);
-            return done(null, false, { message: "DB Error" });
-          });
-      }
-    )
-  );
 };
